727305c154404088c5a5b27e71cbafc32639524c
rhenzala <97076128+rhenzala@users.noreply.github.com>
2025-03-20 12:25:13 +0800

    Add the user state and Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
-import { useState } from 'react'
-import Header from './components/Header'
-import Main from './components/Main'
+import { BrowserRouter as Router } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Header from './components/Header';
+import Main from './components/Main';
 
 function App() {
-  const [showLogin, setShowLogin] = useState(false);
+  const [user, setUser] = useState(() => {
+    return JSON.parse(localStorage.getItem("user")) || null;
+  });
+
+  useEffect(() => {
+    try {
+        const storedUser = localStorage.getItem("user");
+        if (storedUser) {
+            setUser(JSON.parse(storedUser));
+        }
+    } catch (error) {
+        console.error("Error parsing user from localStorage:", error);
+        setUser(null);
+    }
+}, []);
+
 
   return (
-    <div>
-      <Header />
-      <Main />
-    </div>
-  )
+    <Router>
+      <Header user={user} setUser={setUser} />
+      <Main user={user} setUser={setUser} />
+    </Router>
+  );
 }
 
-export default App
+export default App;
